9ff413efe4ade7bbedd3db656bcf5c29dde06747
ys kim <tbvjdkfdir@gmail.com>
2025-06-24 17:57:55 +0900

    [MODIFY] Day 27 - Daily Unity Practice
    [Unity]
    - UI
      - Implemented joystick-based movement input
      - Created jump and attack buttons
    
    - Animation
      - Set up Blend Tree
      - Added animation events
    
    - 2D Platformer
      - Implemented attack, jump, crouch, and crouch-walk mechanics
      - Implemented combo attack system
    
    [Modify]
    --CS - JoystickController
    --CS - KnightController_Joystick
    
    --Scene - Knight
    
    2025/06/24

--- Assets/02.Scripts/Knight/JoystickController.cs
+++ Assets/02.Scripts/Knight/JoystickController.cs
@@ -1,10 +1,48 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class JoystickController : MonoBehaviour
+public class JoystickController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
-    public void OnLog(string param_msg)
+    [SerializeField] private KnightController_Joystick knight_controller;
+
+    [SerializeField] private GameObject background_UI;
+    [SerializeField] private GameObject handler_UI;
+
+   
+
+    private Vector2 start_pos, cur_pos;
+
+    
+
+    void Start()
     {
-        Debug.Log(param_msg);
+        this.background_UI.SetActive(false);
     }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        this.start_pos = eventData.position;
+        this.background_UI.transform.position = start_pos;
+        background_UI.SetActive(true);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.cur_pos = eventData.position;
+        Vector2 drag_dir = cur_pos - start_pos;
+
+        float max_dist = Mathf.Min(drag_dir.magnitude, 100f);
+
+        this.handler_UI.transform.position = this.start_pos + drag_dir.normalized * max_dist;
+        knight_controller.InputJoystick(drag_dir.x, drag_dir.y);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        knight_controller.InputJoystick(0, 0);
+        handler_UI.transform.localPosition = Vector2.zero;
+        background_UI.SetActive(false);
+    }
+
 
 }
--- Assets/02.Scripts/Knight/KnightController_Joystick.cs
+++ Assets/02.Scripts/Knight/KnightController_Joystick.cs
@@ -1,31 +1,46 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KnightController_Joystick : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody2D knight_rb;
 
-    private Vector3 input_dir;
-    [SerializeField]
-    private float move_speed = 3f;
-    private float jump_power = 20f;
+    [SerializeField] private Button jump_btn;
+    [SerializeField] private Button atk_btn;
+
+    [SerializeField] private float move_speed = 3f;
+    [SerializeField] private float jump_power = 20f;
 
     float h, v;
-    bool isFalling = false;
+    private Vector3 input_dir;
 
-    private string anim_bool_isMove = "isRun";
+    bool isFalling = false;
+    bool isCombo = false;
+    bool isAttack = false;
+
+    // private string anim_bool_isMove = "isRun";
     private string anim_trigger_jump = "Jump";
-    private string anim_bool_isfall = "isFall";
+    private string anim_trigger_atk = "Attack";
+
+    private string anim_bool_isFall = "isFall";
+    private string anim_bool_isCombo = "isCombo";
+
+    private string anim_float_joystick_x = "JoystickX";
+    private string anim_float_joystick_y = "JoystickY";
 
     void Start()
     {
         this.animator = this.GetComponent<Animator>();
         this.knight_rb = this.GetComponent<Rigidbody2D>();
+
+        this.jump_btn.onClick.AddListener(this.Jump);
+        this.atk_btn.onClick.AddListener(this.Attack);
     }
 
     void Update() // 일반적인 작업
     {
-        InputKeyboard();
+
     }
 
     private void FixedUpdate() // 물리적인 작업
@@ -34,45 +49,46 @@ public class KnightController_Joystick : MonoBehaviour
         Fall();
     }
 
-    void InputKeyboard()
+    public void InputJoystick(float x, float y)
     {
-        h = Input.GetAxisRaw("Horizontal");
-        v = Input.GetAxisRaw("Vertical");
+        this.input_dir = new Vector3(x, y, 0).normalized;
+        this.animator.SetFloat(anim_float_joystick_x, input_dir.x);
+        this.animator.SetFloat(anim_float_joystick_y, input_dir.y);
 
-        this.input_dir = new Vector3(h, v, 0);
-
-        Jump();
-        SetMoveSprite();
-    }
-
-    private void SetMoveSprite()
-    {
-        if (h != 0)
+        if (input_dir.x != 0)
         {
-            this.animator.SetBool(this.anim_bool_isMove, true);
-
             float scale_x = this.input_dir.x > 0 ? 1 : -1;
             this.transform.localScale = new Vector3(scale_x, 1, 1);
-        }
-        else
-        {
-            this.animator.SetBool(this.anim_bool_isMove, false);
         }
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        this.animator.SetTrigger(this.anim_trigger_jump);
+        this.knight_rb.AddForceY(this.jump_power, ForceMode2D.Impulse);
+    }
+
+    private void Attack()
+    {
+        if (!isCombo)
         {
-            this.animator.SetTrigger(this.anim_trigger_jump);
-            this.knight_rb.AddForceY(this.jump_power, ForceMode2D.Impulse);
+            if (!isAttack)
+            {
+                this.isAttack = true;
+                this.animator.SetTrigger(this.anim_trigger_atk);
+            }
+            else
+                this.isCombo = true;
         }
+
     }
 
     private void Move()
     {
-        if (input_dir.x != 0)
+        if (input_dir.x > 0.45f || input_dir.x < -0.45f)
             this.knight_rb.linearVelocityX = this.input_dir.x * move_speed;
+        else
+            this.knight_rb.linearVelocityX = 0f;
     }
 
     private void Fall()
@@ -81,7 +97,7 @@ public class KnightController_Joystick : MonoBehaviour
             return;
         else if (this.knight_rb.linearVelocityY < 0)
         {
-            this.animator.SetBool(this.anim_bool_isfall, true);
+            this.animator.SetBool(this.anim_bool_isFall, true);
             isFalling = true;
         }
     }
@@ -90,8 +106,30 @@ public class KnightController_Joystick : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            this.animator.SetBool(this.anim_bool_isfall, false);
+            this.animator.SetBool(this.anim_bool_isFall, false);
             this.isFalling = false;
         }
+    }
+
+    private void CheckCombo()
+    {
+        Debug.Log("Check Combo");
+        if (isCombo)
+        {
+            Debug.Log("Combo Attack Execute");
+        }
+        else
+        {
+            Debug.Log("Combo Attack Not Execute");
+        }
+        animator.SetBool(anim_bool_isCombo, isCombo);
+        this.isAttack = false;
+    }
+
+    private void EndComboAttack()
+    {
+        this.isAttack = false;
+        this.isCombo = false;
+        animator.SetBool(anim_bool_isCombo, false);
     }
 }
